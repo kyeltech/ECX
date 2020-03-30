21a1f456349b0a3004bcaf5910ade737fc3f7474
kyeltech <kyel2030@gmail.com>
2020-03-31 00:41:15 +0100

    Day6

--- Day0.js
+++ Day0.js
@@ -1,53 +1,41 @@
-// var today = new Date();
-//   var day = today.getDay();
-//   var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-//   console.log("Today is : " + daylist[day] + ".");
-//   var hour = today.getHours();
-//   var minute = today.getMinutes();
-//   var second = today.getSeconds();
-//   var prepand = (hour >= 12)? " PM ":" AM ";
-//   hour = (hour >= 12)? hour - 12: hour;
-//   if (hour===0 && prepand===' PM ') 
-//   { 
-//   if (minute===0 && second===0)
-//   { 
-//   hour=12;
-//   prepand=' Noon';
-//   } 
-//   else
-//   { 
-//   hour=12;
-//   prepand=' PM';
-//   } 
-//   } 
-//   if (hour===0 && prepand===' AM ') 
-//   { 
-//   if (minute===0 && second===0)
-//   { 
-//   hour=12;
-//   prepand=' Midnight';
-//   } 
-//   else
-//   { 
-//   hour=12;
-//   prepand=' AM';
-//   } 
-//   } 
-// console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
-// var num = '2'
-// console.log(num)
-// console.log('My name is Ezekiel Benjamin and i love coding')
-var http = require("http");http
+var today = new Date();
+  var day = today.getDay();
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log("Today is : " + daylist[day] + ".");
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  var prepand = (hour >= 12)? " PM ":" AM ";
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && prepand===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' PM';
+  } 
+  } 
+  if (hour===0 && prepand===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+var num = '2'
+console.log(num)
+console.log('My name is Ezekiel Benjamin and i love coding')
 
-.createServer(function(req, res) {
 
-console.log(req, res);
-
-res.writeHead(200, { "Content-Type": "text/plain" });
-
-res.write("Hello World!");
-res.end();
-
-})
-
-.listen(8080);
