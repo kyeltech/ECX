7ec18eed524ae9458bd99b9e44df1149a5c26e31
kyeltech <kyel2030@gmail.com>
2020-04-04 16:35:53 +0100

    Day9

--- Day9.js
+++ Day9.js
@@ -1,9 +1,18 @@
 class Car {
-    constructor(CarClass) {
-        this.CarClass = four;
+    constructor() {
+        this.tyres = 4;
     }
-     
+    setTyres(t) {
+        this.tyres = t
+    }
+    displayCarProps(){
+        console.log(`your car has ${this.tyres} tyres`) 
+    }
 }
+    let sportsCar = new Car();
+    sportsCar.setTyres(4)
+    sportsCar.displayCarProps()
+
 
 
 // Write a class method to return the number of tyres of a Car Class
