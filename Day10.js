84b45f015d7b8e9d79ba250e003e34f7feaf6003
kyeltech <kyel2030@gmail.com>
2020-04-04 16:15:12 +0100

    Day10

--- Day10.js
+++ Day10.js
@@ -3,14 +3,12 @@ function valid_email(str)
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
 if(mailformat.test(str))
 {  
-// console.log("Valid email address!");
-return true  
+console.log("Valid email address!");  
 }  
 else  
 {  
-// console.log("You have entered an invalid email address!");  
-return false
+console.log("You have entered an invalid email address!");  
 }
 }
 
-valid_email('me-info@example.com');
+valid_email('me-info@example.com')
